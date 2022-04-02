8df603319b1415dcdbd98b3eec839a8fce46f4d7
Dozer <mail@dozer.cc>
2022-04-02 16:48:55 +0800

    remove cron

--- audio-converter.sh
+++ audio-converter.sh
@@ -8,26 +8,30 @@ fi
 IFS=$'\n'
 FORMATS_LIST=$(echo $SOURCE_VIDEO_FORMATS | tr "," "\n")
 
-echo "audio converter start"
-
-for format in $FORMATS_LIST
+while true
 do
-    for file in $(find "$TARGET" -type f -name "*.$format")
+    echo "========== start =========="
+
+    for format in $FORMATS_LIST
     do
-        echo "analysing: $file"
-        ffprobe_output=$(ffprobe "$file" 2>&1)
-        audio_count=$(echo "$ffprobe_output" | grep "Audio: \w*," | wc -l)
-        target_audio_count=$(echo "$ffprobe_output" | grep "Audio: $TARGET_AUDIO_FORMAT*," | wc -l)
+        for file in $(find "$TARGET" -type f -name "*.$format")
+        do
+            echo "analysing: $file"
+            ffprobe_output=$(ffprobe "$file" 2>&1)
+            audio_count=$(echo "$ffprobe_output" | grep "Audio: \w*," | wc -l)
+            target_audio_count=$(echo "$ffprobe_output" | grep "Audio: $TARGET_AUDIO_FORMAT*," | wc -l)
 
-        if [[ "$audio_count" != "$target_audio_count" ]]; then
-            echo "processing: $file"
-            echo "$ffprobe_output" | grep "Audio: \w*,"
-            new_file=/tmp/$(basename "$file")
-            ffmpeg -i "$file" -map 0:v -c:v copy -map 0:a -c:a ac3 -map 0:s -c:s copy -y "$new_file"
-            chown --reference="$file" "$new_file"
-            mv "$new_file" "$file"
-        fi
+            if [[ "$audio_count" != "$target_audio_count" ]]; then
+                echo "processing: $file"
+                echo "$ffprobe_output" | grep "Audio: \w*,"
+                new_file=/tmp/$(basename "$file")
+                ffmpeg -i "$file" -map 0:v -c:v copy -map 0:a -c:a ac3 -map 0:s -c:s copy -y "$new_file"
+                chown --reference="$file" "$new_file"
+                mv "$new_file" "$file"
+            fi
+        done
     done
-done
 
-echo "audio converter finish"
+    echo "========== finish ========="
+    sleep $INTERVAL
+done
